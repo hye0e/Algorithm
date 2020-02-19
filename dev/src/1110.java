547820750960eacbf0c48685dee5525acff8dcff
hyshin <nanda5050@naver.com>
2020-02-19 17:47:30 +0900

    1110번 - 더하기 사이클

---
+++ dev/src/1110.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+        int origin = input;
+        int count = 0;
+
+        while (true) {
+            int newNum = ((input % 10) * 10 ) + ((input / 10) + (input % 10)) % 10;
+            input = newNum;
+            count++;
+            if (origin == newNum) {
+                break;
+            }
+        }
+        System.out.println(count);
+    }
+}
+
