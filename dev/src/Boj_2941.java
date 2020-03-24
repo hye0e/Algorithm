59a3c401344c9b89bf15882bd83ba9539e163bec
hyshin <nanda5050@naver.com>
2020-03-24 22:17:17 +0900

    2941 - 크로아티아 알파벳

--- dev/src/Boj_2941.java
+++ dev/src/Boj_2941.java
@@ -3,30 +3,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * 경
- * č	c=
- * ć	c-
- * dž	dz=
- * đ	d-
- * lj	lj
- * nj	nj
- * š	s=
- * ž	z=
+ *예를 들어, ljes=njak은 크로아티아 알파벳 6개(lj, e, š, nj, a, k)로 이루어져 있다.
+ * 단어가 주어졌을 때, 몇 개의 크로아티아 알파벳으로 이루어져 있는지 출력한다.
+ *
+ * dž는 무조건 하나의 알파벳으로 쓰이고, d와 ž가 분리된 것으로 보지 않는다.
+ * lj와 nj도 마찬가지이다. 위 목록에 없는 알파벳은 한 글자씩 센다.
  */
 public class Boj_2941 {
     // ljes= njak
     public static void main(String[] args) throws IOException {
-        String[] alpArr = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+        String[] wordArr = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split("=");
+        String input = br.readLine().trim();
 
-        int cnt = 0;
-        int charNum = 0;
-        // 위 목록에 없는 알파벳은 한 글자씩 센다. // lj e s= nj a k
-        for (int i = 0; i < input.length; i++) {
-           charNum += input[i].charAt(0);
-
+        for (int i = 0; i < wordArr.length; i++) {
+            if (input.contains(wordArr[i])) {
+                input = input.replaceAll(wordArr[i], "*");
+            }
         }
-
+        System.out.println(input.length());
     }
 }
