ff793b6d313ca3a867ce2096da1b7fea6e753a3d
hyshin <nanda5050@naver.com>
2020-03-31 23:05:57 +0900

    2869 - 달팽이는 올라가고 싶다

---
+++ dev/src/Boj_2869.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.Scanner;
+
+public class Boj_2869 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int v = sc.nextInt();
+
+        int cnt = 0;
+        int sum = 0;
+
+        while (sum != v) {
+            sum += a;
+            if (sum != v) {
+                sum -= b;
+            }
+            cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
