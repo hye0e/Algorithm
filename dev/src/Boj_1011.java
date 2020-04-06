40360a102092cb62b63f26e1b6c00e936a4d9562
hyshin <nanda5050@naver.com>
2020-04-06 23:11:36 +0900

    1011 - Fly me to the Alpha Centauri

--- dev/src/Boj_1011.java
+++ dev/src/Boj_1011.java
@@ -2,33 +2,20 @@ import java.util.Scanner;
 
 public class Boj_1011 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long testCase = sc.nextInt();
-        StringBuilder sb = new StringBuilder();
+        Scanner in = new Scanner(System.in);
+        long case_count = in.nextInt();
+        for (long i = 0; i < case_count; i++) {
+            long x_pos = in.nextInt();
+            long y_pos = in.nextInt();
+            long length = y_pos - x_pos;
+            long answer = 1;
+            long dis = 0;
 
-        for (long i =  0; i < testCase; i++) {
-            long x = sc.nextLong();
-            long y = sc.nextLong();
-
-            long[] arr = distanceCal(x);
-            long cnt = x == 0 ? 1 : 0;
-
-            while (arr[2] != y) {
-                arr = distanceCal(arr[2]);
-                cnt++;
+            while (length > dis) {
+                answer++;
+                dis += answer / 2;
             }
-
-            sb.append(cnt + "\n");
+            System.out.println(answer - 1);
         }
-        System.out.println(sb);
-    }
-
-    public static long[] distanceCal (long input) {
-        long[] result = new long[3];
-        result[0] = input - 1;
-        result[1] = input;
-        result[2] = input + 1;
-
-        return result;
     }
 }
