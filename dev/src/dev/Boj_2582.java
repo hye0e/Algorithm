65119af5e786a295d8c408ee68da3c7276196b89
hyshin <nanda5050@naver.com>
2020-04-13 19:43:41 +0900

    2581 - 소수

--- dev/src/dev/Boj_2582.java
+++ dev/src/dev/Boj_2582.java
@@ -1,18 +1,35 @@
 package dev;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Boj_2582 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//        int min = sc.nextInt();
-//        int max = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        int min = sc.nextInt();
+        int max = sc.nextInt();
 
-        ArrayList<Integer> primeList = isPrimeList(1, 20);
+        ArrayList<Integer> primeList = isPrimeList(1, max);
+        ArrayList<Integer> result = new ArrayList<>();
+        int sum = 0;
         for (int prime : primeList) {
-            System.out.println(prime);
+            if (prime >= min && prime <= max ) {
+                sum += prime;
+                result.add(prime);
+            }
         }
+
+        if (!result.isEmpty()) {
+            Collections.sort(result);
+            sb.append(sum + "\n");
+            sb.append(result.get(0));
+        } else {
+            sb.append("-1");
+        }
+
+        System.out.println(sb);
     }
     static ArrayList<Integer> isPrimeList (int min, int max) {
         ArrayList<Integer> primeList = new ArrayList<>();
