16e08f9483de98cc26fe54950ad2bdfb3ca0fdb8
hyshin <nanda5050@naver.com>
2020-04-23 23:03:11 +0900

    1022 - 터렛 ing

---
+++ dev/src/dev/Boj_1002.java
@@ -0,0 +1,13 @@
+package dev;
+
+import java.util.Scanner;
+
+public class Boj_1002 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testCnt = sc.nextInt();
+        for (int i = 0; i < testCnt; i++) {
+
+        }
+    }
+}
