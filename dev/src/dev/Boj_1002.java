088de138116e083e767572344e8a8adc6d39c79a
hyshin <nanda5050@naver.com>
2020-04-24 23:42:39 +0900

    1022 - 터렛 ing

--- dev/src/dev/Boj_1002.java
+++ dev/src/dev/Boj_1002.java
@@ -6,8 +6,19 @@ public class Boj_1002 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int testCnt = sc.nextInt();
+        // x1, y1, r1, x2, y2, r2
         for (int i = 0; i < testCnt; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+        }
+    }
 
+    public static class circle {
+        int x;
+        int y;
+        public circle(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 }
