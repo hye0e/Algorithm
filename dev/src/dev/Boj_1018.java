c33ce4a7b9fea2f8d3eea46d190b2c660372c0c2
hyshin <nanda5050@naver.com>
2020-05-12 23:08:03 +0900

    1018 - 체스판 다시 칠하기 -ing

--- dev/src/dev/Boj_1018.java
+++ dev/src/dev/Boj_1018.java
@@ -3,15 +3,16 @@ package dev;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Boj_1018 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int width = Integer.parseInt(br.readLine());
-        int height = Integer.parseInt(br.readLine());
+        Scanner sc = new Scanner(System.in);
+        int width = sc.nextInt();
+        int height = sc.nextInt();
         for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; i++) {
-
+            for (int j = 0; j < height; j++) {
+                System.out.println(i + "," + j);
             }
         }
     }
