75aca136816faac1a1b59ad9d328cbe381585477
hye0 <nanda5050@naver.com>
2021-03-13 23:49:48 +0900

    일일코테 0313

---
+++ dev/src/dev/Boj_10757.java
@@ -0,0 +1,14 @@
+package dev;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Boj_10757 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split(" ");
+        System.out.println(new BigInteger(arr[0]).add(new BigInteger(arr[1])));
+    }
+}
