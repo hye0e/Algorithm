504dbef2967bb9f402a869f09e8e2896992d78e1
hye0e <nanda5050@naver.com>
2021-04-24 14:31:55 +0900

    [프로그래머스] 모의고사

--- dev/src/programmers/Prgmers_0424.java
+++ dev/src/programmers/Prgmers_0424.java
@@ -10,44 +10,27 @@ public class Prgmers_0424 {
         int[] student2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] student3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-        student1 = formatAnswer(answers.length, student1);
-        student2 = formatAnswer(answers.length, student2);
-        student3 = formatAnswer(answers.length, student3);
+        int[] answer = new int[3];
 
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(answerCheck(student1, answers));
-        arr.add(answerCheck(student2, answers));
-        arr.add(answerCheck(student3, answers));
-            // 1번 비교 == 2번 비교 -> answer[0] = 1, 2
-            // 2번 비교 == 3번 비 -> answer[0] = 1, 2, 3
-            return new int[]{1, 2,3};
-    }
-    private static int answerCheck(int[] studentArr, int[] answer) {
-        int correctCnt = 0;
-        for(int i = 0; i < studentArr.length; i++) {
-            if(studentArr[i] == answer[i]) {
-                correctCnt++;
-            }
-        }
-        return correctCnt;
-    }
-    private static int[] formatAnswer(int answerCnt, int[] studentAnswer) {
-        int[] formatArr = new int[answerCnt];
-
-        for(int j = 0; j < studentAnswer.length; j++) {
-            if (j < answerCnt) {
-                formatArr[j] = studentAnswer[j];
-            }
+        for(int i = 0; i < answers.length; i++) {
+            if (answers[i] == student1[i%student1.length]) answer[0]++;
+            if (answers[i] == student2[i%student2.length]) answer[1]++;
+            if (answers[i] == student3[i%student3.length]) answer[2]++;
         }
 
-        for(int i = studentAnswer.length; i < answerCnt; i++) {
-            formatArr[i] = studentAnswer[i%studentAnswer.length];
-        }
-        return formatArr;
+        int max = Math.max(Math.max(answer[0], answer[1]), answer[2]);
+
+        ArrayList<Integer> answerList = new ArrayList<>();
+        if(answer[0] == max) answerList.add(1);
+        if(answer[1] == max) answerList.add(2);
+        if(answer[2] == max) answerList.add(3);
+
+        return answerList.stream().mapToInt(i -> i.intValue()).toArray();
     }
 
     @Test
     public void 정답체크() {
-        Assert.assertEquals(new int[]{1, 2, 3, 4, 5}, solution(new int[]{1,2,3,4,5}));
+//        Assert.assertEquals(new int[]{1}, solution(new int[]{1, 2, 3, 4, 5}));
+        Assert.assertEquals(new int[]{1, 2, 3}, solution(new int[]{1, 3, 2, 4, 2}));
     }
 }
