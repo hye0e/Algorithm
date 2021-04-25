2bdbab801be51677329d47c8225456035c754982
hye0e <nanda5050@naver.com>
2021-04-25 16:13:52 +0900

    python

---
+++ dev/src/python/skill_tree.py
@@ -0,0 +1,17 @@
+def solution(skill, skill_trees):
+    answer = 0
+
+    for skills in skill_trees:
+        skill_list = list(skill)
+
+        for s in skills:
+            if s in skill:
+                if s != skill_list.pop(0):
+                    break
+        else:
+            answer += 1
+
+    return answer
+if __name__ == '__main__':
+    arr = ["BACDE", "CBADF", "AECB", "BDA"]
+    print(solution('CBD', arr))
