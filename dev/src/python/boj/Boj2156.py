d460c486ef7630a9ed606bb7f239ee7ecdbca531
hye0e <nanda5050@naver.com>
2021-05-21 23:49:53 +0900

    [0521]백준

--- dev/src/python/boj/Boj2156.py
+++ dev/src/python/boj/Boj2156.py
@@ -1,11 +1,17 @@
-cnt = int(input())
-l = []
-for i in range(0, cnt):
-    k = int(input())
-    l.append(k)
+n = int(input())
+wine_list = [int(input()) for x in range(n)]
 
-l[0]+=l[1]
-l[1]+=l[2]
-for j in range(2, len(l)):
-    l[j]+=max(l[j-1], l[j-2])
+dp = [0]
+dp.append(wine_list[0])
+if(n > 1):
+    dp.append(wine_list[0] + wine_list[1])
 
+for i in range(3, n + 1):
+    case_1 = wine_list[i - 1] + dp[i - 2]
+    case_2 = wine_list[i - 1] + wine_list[i - 2] + dp[i - 3]
+    case_3 = dp[i - 1]
+    max_value = max(case_1, case_2, case_3)
+    
+    dp.append(max_value)
+    
+print(dp[n])
