824a43de01b4c49d8e9506ba7495ccdd4cb904eb
hye0e <nanda5050@naver.com>
2021-05-25 01:15:11 +0900

    0525백준

--- dev/src/python/boj/Boj11722.py
+++ dev/src/python/boj/Boj11722.py
@@ -1,10 +1,9 @@
 n = int(input())
 arr = list(map(int, input().split()))
 dp = [1 for _ in range(n)]
-
 for i in range(1, n):
     for j in range(i):
-        if arr[i] > arr[j]:
-            dp[j] = max(dp[j], dp[j]+1)
+        if arr[i] < arr[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
 
 print(max(dp))
