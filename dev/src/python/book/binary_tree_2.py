37c06a80b1b31decc30d45b897168935ef000d88
hye0e <nanda5050@naver.com>
2021-06-01 17:43:44 +0900

    0601일일코테

---
+++ dev/src/python/book/binary_tree_2.py
@@ -0,0 +1,28 @@
+import sys
+
+def binary(search, key):
+    low = 0
+    high = len(search) - 1
+    while low <= high:
+        mid = (low+high) // 2
+
+        if key == search[mid]:
+            return True
+        elif key < search[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+    
+    return False
+
+n = int(input())
+n_list = list(map(int, sys.stdin.readline().rstrip().split()))
+
+m = int(input())
+m_list = list(map(int, sys.stdin.readline().rstrip().split()))
+
+for i in range(len(m_list)):
+    if binary(n_list, m_list[i]):
+        print('yes', end = '')
+    else:
+        print('No', end = '')
