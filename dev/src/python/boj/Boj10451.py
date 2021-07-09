643714368f3e194336c7e381346b9881d252a87e
hye0e <nanda5050@naver.com>
2021-07-09 19:11:41 +0900

    [feat] dfs/bfs

--- dev/src/python/boj/Boj10451.py
+++ dev/src/python/boj/Boj10451.py
@@ -1,7 +1,26 @@
-visited = []
-queue = []
+#-*- coding:utf-8 -*-
+# 1. visited 방문하면 True
+# 2. 이미 방문한 곳이면 탈출
+# 3. result 더해주기
+import sys
+sys.setrecursionlimit(2000)
 
-dic = {'1': [3, 5], '3': [1, 7], '5': [1, 7], '7': [3, 5]}
+def dfs(num):
+    visited[num] = True
+    val = num_list[num]
+    if not visited[val]:
+        dfs(val)
 
-print(dic.items)
-print(dic.keys)
+N = int(sys.stdin.readline())
+for _ in range(N):
+    index = int(sys.stdin.readline())
+    result = 0
+
+    visited = [True] + [False] * index
+    num_list = [0] + list(map(int, sys.stdin.readline().split()))
+
+    for i in range(1, index + 1):
+        if not visited[i]:
+            dfs(i)
+            result += 1
+    print(result)   
