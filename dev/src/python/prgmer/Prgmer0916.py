c0261227cc3f08dbe228bf2c3567e3e0eb307a89
hye0e <nanda5050@naver.com>
2021-09-19 22:24:57 +0900

    0919

---
+++ dev/src/python/prgmer/Prgmer0916.py
@@ -0,0 +1,10 @@
+def solution(lottos, win_nums):
+    cnt_arr = [0, 6, 5, 4, 3, 2, 1]
+    answer = []
+    sorted(lottos)
+    sorted(win_nums)
+
+    cnt = 0
+    # 0의 개수
+    # 맞은 개수
+    return answer
