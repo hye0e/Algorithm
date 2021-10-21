d418d76b79ade4f28ec2d9e896a9277bd5a0a924
hye0e <nanda5050@naver.com>
2021-10-21 21:00:33 +0900

    1021

---
+++ dev/src/python/boj/11651.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+num_list = [input().split() for _ in range(N)]
+
+
+num_list.sort(key = lambda x: (int(x[1]),int(x[0])))
+
+for i in num_list:
+    print(i[0], i[1])
+print(num_list)
