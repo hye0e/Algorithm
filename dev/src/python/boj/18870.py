be522f7524084098dad543bd3e62eb07b753e9d5
hye0e <nanda5050@naver.com>
2021-10-23 23:51:59 +0900

    1023

--- dev/src/python/boj/18870.py
+++ dev/src/python/boj/18870.py
@@ -3,4 +3,18 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-arr = [_ for _ in N]
+arr = list(map(int, input().split()))
+
+dict = {}
+sort_arr = sorted(arr)
+index = 0
+for i in sort_arr:
+    dict[i] = index
+    index += 1
+
+for i in arr:
+    for key, value in enumerate(dict):
+        if i == value:
+            print(key, end = '')
+print(arr)
+print(sort_arr)
