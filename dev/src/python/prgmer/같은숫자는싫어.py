73d5a12ae3a0e438e7b67d76c88b9398907b108a
hye0e <nanda5050@naver.com>
2021-11-19 23:55:48 +0900

    1119

--- dev/src/python/prgmer/같은숫자는싫어.py
+++ dev/src/python/prgmer/같은숫자는싫어.py
@@ -2,8 +2,8 @@ from dev.src.python.prgmer.없는숫자더하기 import solution
 
 
 def solution(arr):
-    answer = list(set(arr))
-    return answer
+    answer = []
+
 
 if __name__ == '__main__':
     print(solution([1,1,3,3,0,1,1]))
