2fcb7c11a6f83827e4f13452e88367b3dff39d30
hye0e <nanda5050@naver.com>
2022-09-01 14:05:45 +0900

    펠린드롬

--- implementaion/1259.py
+++ implementaion/1259.py
@@ -1,9 +1,3 @@
-"""
-121
-1231
-12421
-0
-"""
 import sys
 
 input = sys.stdin.readline
@@ -13,7 +7,7 @@ while True:
 
     if num == '0':
         break
-    
+
     if num == num[::-1]:
         print('yes')
     else:
