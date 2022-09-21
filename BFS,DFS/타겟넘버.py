71fb3c35681fa3bce10a3f2f04b61ec80ddfc221
hye0e <nanda5050@naver.com>
2022-09-21 19:29:39 +0900

    타겟넘버

---
+++ BFS,DFS/타겟넘버.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+def solution(numbers, target):
+    answer = 0
+    queue = deque()
+    queue.append([numbers[0], 0])
+    queue.append([-1 * numbers[0], 0])
+    stop = len(numbers)
+    while queue:
+        temp, idx = queue.popleft()
+        idx += 1
+        if idx < stop:
+            queue.append([temp + numbers[idx], idx])
+            queue.append([temp - numbers[idx], idx])
+        else:
+            if temp == target:
+                answer += 1
+
+    return answer
+
+if __name__ == '__main__':
+    print(solution([1, 1, 1, 1], 3))
