970852fa8b889ac18fbab577b1d1d211602abd33
hye0e <nanda5050@naver.com>
2022-12-24 01:29:47 +0900

    BOJ/Silver3

--- BruteForce/14501.py
+++ BruteForce/14501.py
@@ -27,17 +27,7 @@ for i in range(N):
 dp.append(0)
 
 answer = 0
-for i in range(N - 1, -1, -1):
-    if t_list[i] + i > N:
-        dp[i] = dp[i + 1]
-    else:
-        print(dp[i + t_list[i]])
-        print(t_list[i])
-        dp[i] = max(dp[i + 1], p_list[i] +
-                    dp[i + t_list[i]])
 
-
-# print(answer)
 
 
 
