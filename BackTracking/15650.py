eff26b779cd1fdf7970e44c2bd4d369770f828da
hye0e <nanda5050@naver.com>
2023-01-12 09:23:27 +0900

    BOJ/Silver3

---
+++ BackTracking/15650.py
@@ -0,0 +1,20 @@
+"""
+11시까지
+"""
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().strip().split())
+arr = []
+def dfs(start, cnt):
+    if cnt == M:
+        print(' '.join(arr))
+        return
+
+    for i in range(start, N + 1):
+        arr.append(str(i))
+        dfs(i + 1, cnt + 1)
+        arr.pop()
+
+dfs(1, 0)
