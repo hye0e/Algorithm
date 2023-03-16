a2ec40f0844b19d9436e7b7960868f66598a2a5c
HyeYoung Shin <nanda5050@naver.com>
2023-03-16 16:36:05 +0900

    [Silver I] Title: 회의실 배정, Time: 568 ms, Memory: 44808 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -6,29 +6,31 @@ import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Main {
-	 static class MeetingRoom implements Comparable<MeetingRoom> {
-		int start;
-		int end;
+class MeetingRoom implements Comparable<MeetingRoom> {
+	int start;
+	int end;
 
-		public MeetingRoom(int start, int end) {
-			this.start = start;
-			this.end = end;
-		}
-
-		@Override
-		public String toString() {
-			return "MeetingRoom [start=" + start + ", end=" + end + "]";
-		}
-
-		// 단, 회의는 한번 시작하면 중간에 중단될 수 없으며 한 회의가 끝나는 것과 동시에 다음 회의가 시작될 수 있다. 
-		// 회의의 시작시간과 끝나는 시간이 같을 수도 있다.
-		@Override
-		public int compareTo(MeetingRoom o) {
-			return this.end - o.end;
-		}
+	public MeetingRoom(int start, int end) {
+		this.start = start;
+		this.end = end;
 	}
 
+	@Override
+	public String toString() {
+		return "MeetingRoom [start=" + start + ", end=" + end + "]";
+	}
+
+	// 단, 회의는 한번 시작하면 중간에 중단될 수 없으며 한 회의가 끝나는 것과 동시에 다음 회의가 시작될 수 있다. 
+	// 회의의 시작시간과 끝나는 시간이 같을 수도 있다.
+	@Override
+	public int compareTo(MeetingRoom o) {
+		if (this.end == o.end) return this.start - o.start;
+		
+		return this.end - o.end;
+	}
+}
+
+public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
@@ -41,7 +43,7 @@ public class Main {
 
 			timeTable.add(new MeetingRoom(start, end));
 		}
-		Collections.sort(timeTable, (a, b) -> a.start - b.start);
+//		Collections.sort(timeTable, (a, b) -> a.start - b.start);
 		Collections.sort(timeTable);
 		
 		int selectedEnd = timeTable.get(0).end;
