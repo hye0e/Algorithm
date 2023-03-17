75bd0ba6acf5b7bad5617d2bec7cea085c0f7b6f
HyeYoung Shin <nanda5050@naver.com>
2023-03-17 18:48:17 +0900

    [Gold V] Title: 신기한 소수, Time: 1472 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -4,39 +4,37 @@ import java.io.InputStreamReader;
 
 public class Main {
 	static int N;
-	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
 		
+		N = Integer.parseInt(br.readLine());
 		dfs(2, 1);
 		dfs(3, 1);
 		dfs(5, 1);
 		dfs(7, 1);
+//		System.out.println(isPrime(5));
+	
 	}
 	
-	private static boolean isPrime(int num) {
+	public static boolean isPrime(int num) {
 		for (int i = 2; i < num; i++) {
-			if (num % i == 0) return false; 
+			if (num % i == 0) return false;
 		}
 		return true;
 	}
 	
-	private static void dfs(int num, int cnt) {
-		if (cnt == N) {
-			if (isPrime(num)) {
-				System.out.println(num);
-			}
-			
+	public static void dfs(int num, int temp) {	
+		if (temp == N) {
+			if (!isPrime(num)) return;
+			System.out.println(num);
 			return;
 		}
 		
 		for (int i = 1; i <= 9; i++) {
 			if (i % 2 == 0) continue;
-			
-			if (isPrime(num * 10 + i)) {
-				dfs(num * 10 + i, cnt+1);
-			}
+			int tempNum = Integer.parseInt(String.valueOf(num) + String.valueOf(i)); 
+			if (!isPrime(tempNum)) continue;
+			dfs(tempNum, temp+1);
 		}
 	}
 
