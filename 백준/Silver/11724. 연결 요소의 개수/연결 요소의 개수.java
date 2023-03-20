ef6478a5f1a25b8136f052596960c9bc88e6490c
HyeYoung Shin <nanda5050@naver.com>
2023-03-20 13:26:54 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 692 ms, Memory: 142024 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -2,57 +2,52 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	static List<List<Integer>> graph = new ArrayList<>();
-	static boolean[] isVisited;
-	
+	static ArrayList<Integer>[] graph;
+	static boolean[] visited;
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		isVisited = new boolean[N + 1];
+		graph = new ArrayList[N+1]; // 0번 노드부터 시작
+		visited = new boolean[N+1];
 		
-		for(int i = 0; i < N+1; i++) {
-			graph.add(new ArrayList<>());
+		for (int i = 0; i < N + 1; i++) {
+			graph[i] = new ArrayList<>();
 		}
-		
+
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int node = Integer.parseInt(st.nextToken());
-			int edge = Integer.parseInt(st.nextToken());
-
-			graph.get(node).add(edge);
-			graph.get(edge).add(node);
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			
+			graph[from].add(to);
+			graph[to].add(from);
 		}
 		
 		int answer = 0;
-		for (int i = 1; i < graph.size(); i++) {
-			if (!isVisited[i]) {
+		for (int i = 1; i < N+1; i++) {
+			if (!visited[i]) {
 				answer++;
-				dfs(i);
+				DFS(i);
 			}
 		}
 		
 		System.out.println(answer);
 	}
 	
-	public static void dfs(int x) {
-		if (isVisited[x]) return;
-		
-		isVisited[x] = true;
-		
-		for (int i = 0; i < graph.get(x).size(); i++) {
-			if (!isVisited[graph.get(x).get(i)]) {
-				dfs(graph.get(x).get(i));
+	public static void DFS(int currNode) {
+		if (visited[currNode]) return;
+		visited[currNode] = true;
+		for (int node: graph[currNode]) {
+			if (!visited[node]) {
+				DFS(node);
 			}
-		}
+		}		
 	}
 
 }
