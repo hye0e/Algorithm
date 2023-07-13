2f8e17bf952db46fd1cd2c77f46f1b7775e0db3e
HyeYoung Shin <nanda5050@naver.com>
2023-07-13 11:27:24 +0900

    [Gold V] Title: 암호 만들기, Time: 128 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,72 +1,72 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int L, C;
+    static int L, C;
+    static int[] selected;
+    static boolean[] visited;
+    static char[] password;
+    static StringBuilder stringBuilder = new StringBuilder();
 
-	static char[] arr;
-	static char[] password;
-	
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		L = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		
-		arr = new char[C];
-		
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < C; i++) {
-			arr[i] = st.nextToken().charAt(0);
-		}
-		
-		Arrays.sort(arr);
-		password = new char[L];
-		
-		dfs(0, 0);
-		System.out.println(sb);
-	}
-	
-	private static boolean check() {
-		int aeiouCnt = 0;
-		int restCnt = 0;
-		for (int i = 0; i < password.length; i++) {
-			if (password[i] == 'a' || password[i] == 'e' || password[i] == 'i' || 
-					password[i] == 'o' || password[i] == 'u' ) {
-				aeiouCnt += 1;
-			} else {
-				restCnt += 1;
-			}
-		}
-		
-		if (aeiouCnt >= 1 && restCnt >= 2) {
-			return true;
-		}
-		return false;
-	}
-	
-	private static void dfs(int cnt, int startIndex) {
-		if (cnt == L) {
-			if (check()) {
-				for (char c: password) {
-					sb.append(c);
-				}
-				sb.append("\n");
-			}
+    public static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-			return;
-		}
-		
-		for (int i = startIndex; i < C; i++) {
-			password[cnt] = arr[i];
-			dfs(cnt + 1, i + 1);
-		}
-	}
+        L = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        password = new char[C];
+        selected = new int[L];
+        visited = new boolean[C];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < C; i++) {
+            password[i] = st.nextToken().charAt(0);
+        }
+
+        Arrays.sort(password);
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        dfs(0, 0);
+        System.out.println(stringBuilder);
+    }
+
+    public static boolean valid() {
+        int aeiou = 0;
+        int elseCnt = 0;
+
+        for (int i = 0; i < L; i++) {
+            if (password[selected[i]] == 'a' || password[selected[i]] == 'e' || password[selected[i]] == 'i' || password[selected[i]] == 'o' || password[selected[i]] == 'u') {
+                aeiou++;
+            } else {
+                elseCnt++;
+            }
+        }
+
+        if (aeiou >= 1 && elseCnt >= 2) {
+            return true;
+        }
+        return false;
+    }
+    public static void dfs(int start, int depth) {
+        if (depth == L) {
+            // a, e, i, o, u 체크
+            if (valid()) {
+                for (int i = 0; i < L; i++) stringBuilder.append(password[selected[i]]);
+                stringBuilder.append("\n");
+//                System.out.println(Arrays.toString(selected));
+            }
+        } else {
+            // 순서O 중복X
+            for (int i = start; i < C; i++) {
+                selected[depth] = i;
+                dfs(i + 1, depth + 1);
+                selected[depth] = 0;
+            }
+        }
+    }
 }
