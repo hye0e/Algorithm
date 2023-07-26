c4babde905452eae6b3452e948089c99f48fe1af
HyeYoung Shin <nanda5050@naver.com>
2023-07-26 17:42:19 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1808 ms, Memory: 156480 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            queue.add(Integer.parseInt(br.readLine()));
+        }
+
+        while (!queue.isEmpty()) {
+            sb.append(queue.poll()).append("\n");
+//            System.out.println(queue.poll());
+        }
+        System.out.println(sb);
+    }
+}
