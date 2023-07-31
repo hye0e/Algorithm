3f28c3eae43d48dd5749d0f4a418c6f6b4c08e18
HyeYoung Shin <nanda5050@naver.com>
2023-08-01 08:10:13 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.34 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        boolean[] visited = new boolean[10];
+        Arrays.sort(numbers);
+        
+        for (int i = 0; i < numbers.length; i++) {
+            visited[numbers[i]] = true;
+        }
+        
+        for (int i = 0; i < visited.length; i++) {
+            if (!visited[i]) answer += i;
+        }
+        
+        return answer;
+    }
+}
