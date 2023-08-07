b498ab350cfadec5736a8a600ad9ebda6b5d882c
HyeYoung Shin <nanda5050@naver.com>
2023-08-07 16:33:04 +0900

    [Gold V] Title: 두 용액, Time: 380 ms, Memory: 29848 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -5,46 +5,41 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int[] arr;
-    static int diff = Integer.MAX_VALUE;
-    static int answer1, answer2;
-    public static void input() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i ++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(arr);
-    }
-
-    public static void solve() {
-        int start = 0;
-        int end = arr.length - 1;
-
-        int internalSum;
-        while (start < end) {
-            internalSum = Math.abs(arr[start] + arr[end]);
-            if (internalSum < diff) {
-                diff = internalSum;
-                answer1 = arr[start];
-                answer2 = arr[end];
-            }
-
-            // 양수면 오른쪽 방향을 좀 더 음수쪽으로
-            if (arr[start] + arr[end] > 0) end--;
-            // 음수면 왼쪽 방향을 좀 더 양수족으로
-            else start++;
-        }
-        System.out.println(answer1 + " " + answer2);
-    }
 
     public static void main(String[] args) throws IOException {
-        input();
-        solve();
+        int answer = Integer.MAX_VALUE;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] num = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(num);
+
+        int i = 0;
+        int j = num.length - 1;
+
+        int num1 = 0;
+        int num2 = 0;
+
+        int temp = Integer.MAX_VALUE;
+        while (i < j) {
+            int mixed = Math.abs(num[i] + num[j]);
+
+            if (mixed < temp) {
+                temp = mixed;
+                num1 = num[i];
+                num2 = num[j];
+            }
+            if (num[i] + num[j] < 0) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        System.out.println(num1 + " " + num2);
     }
 }
