a1368968c1407b4251a7e5b1165001f4ae63cee6
HyeYoung Shin <nanda5050@naver.com>
2023-08-09 14:52:15 +0900

    [unrated] Title: 호텔 대실, Time: 7.42 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.java
@@ -18,7 +18,7 @@ class Solution {
         Arrays.sort(table, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                if (o1[0] == o2[0]) return o1[1] - o2[1];
+                //if (o1[0] == o2[0]) return o1[1] - o2[1];
                 return o1[0] - o2[0];
             }
         });
