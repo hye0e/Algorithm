9545fc0cdfa88f2c4012ed81ca834f1fe0422734
HyeYoung Shin <nanda5050@naver.com>
2023-08-14 21:21:58 +0900

    [unrated] Title: 리코쳇 로봇, Time: 0.69 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/unrated/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/unrated/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,86 +1,86 @@
 import java.util.*;
 
 class Solution {
-    static int N, M; // 가로, 세로
-    static char[][] graph;
-    static boolean[][] visited;
-    static int goalX, goalY;
-    static int answer; 
+    int N, M;
+    char[][] graph;
+    boolean[][] visited;
     
-    public class Node {
-        int x, y, cnt;
-        public Node(int x, int y, int cnt) {
+    char GOAL = 'G';
+    char START = 'R';
+    char DISABLE = 'D';
+    
+    int answer;
+    
+    class Node {
+        int x, y, depth;
+        public Node(int x, int y, int depth) {
             this.x = x;
             this.y = y;
-            this.cnt = cnt;
+            this.depth = depth;
         }
     }
     
     public int solution(String[] board) {
+        // 1. bfs 활용
+        // 2. 게임판 위의 장애물이나 맨 끝에 부딪힐 때까지 미끄러져 이동하는 것 -> while
         N = board.length;
         M = board[0].length();
-        
         graph = new char[N][M];
         visited = new boolean[N][M];
         
-        for (int i = 0; i < N; i++) {
-            char[] temp = board[i].toCharArray();
-            for (int j = 0; j < temp.length; j++) {
-                if (temp[j] == 'G') {
-                    goalX = i;
-                    goalY = j;
-                }
-                graph[i][j] = temp[j];
-            }
+        for (int i = 0; i < board.length; i++) {
+           char[] temp = board[i].toCharArray();
+           graph[i] = temp;
         }
-
+        
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (graph[i][j] == 'R') {
-                    answer = bfs(i, j);   
-                }
+                if (graph[i][j] == START) answer = bfs(i, j);
             }
         }
-                
+        
+        // System.out.println(Arrays.deepToString(graph));
         return answer;
     }
     
     public int bfs(int x, int y) {
         Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(x, y, 0));
-        
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
-
-        while(!queue.isEmpty()) {
+        
+        queue.add(new Node(x, y, 0));
+        visited[x][y] = true;
+        
+        while (!queue.isEmpty()) {
             Node node = queue.poll();
-            
-            if (node.x == goalX && node.y == goalY) {
-                return node.cnt;
-            }
+            if (graph[node.x][node.y] == GOAL) return node.depth;
             
             for (int i = 0; i < 4; i++) {
                 int nx = node.x;
                 int ny = node.y;
-                
-                
-                while ((0 <= nx && nx < N && 0 <= ny && ny < M) &&
-                    graph[nx][ny] != 'D') {
+
+//                 if (visited[nx][ny]) continue;
+
+                while (isRange(nx, ny) && graph[nx][ny] != DISABLE) {
                     nx += dx[i];
-                    ny += dy[i];
-                }
-                
+                    ny += dy[i];    
+                } 
                 
                 nx -= dx[i];
                 ny -= dy[i];
                 
                 if (visited[nx][ny]) continue;
-
-                queue.add(new Node(nx, ny, node.cnt + 1));
+                
+                queue.add(new Node(nx, ny, node.depth + 1));
                 visited[nx][ny] = true;
             }
         }
         
         return -1;
+    }
+    
+    public boolean isRange(int x, int y) {
+        if (0 <= x && x < N && 0 <= y && y < M) return true;
+        return false;
     }
 }
