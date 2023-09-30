c810b6316af78568b03b1b9111c040cc702d4583
HyeYoung Shin <nanda5050@naver.com>
2023-09-30 23:55:51 +0900

    [level 1] Title: 모의고사, Time: 3.48 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+        
+        HashMap<Integer, Integer> board = new HashMap<>();
+        
+        int[] stu1 = new int[]{1, 2, 3, 4, 5};
+        int[] stu2 = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
+        int[] stu3 = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        
+        int cnt = answers.length;
+        
+        for (int i = 0; i < answers.length; i++) {
+            if (stu1[i % stu1.length] == answers[i]) board.put(1, board.getOrDefault(1, 0) + 1);
+            if (stu2[i % stu2.length] == answers[i]) board.put(2, board.getOrDefault(2, 0) + 1);
+            if (stu3[i % stu3.length] == answers[i]) board.put(3, board.getOrDefault(3, 0) + 1);
+        }
+        
+        int max = 0;
+        for (Integer key : board.keySet()) {
+            max = Math.max(max, board.get(key));
+        }
+        
+        List<Integer> ans = new ArrayList<>();
+        
+        for (Integer key : board.keySet()) {
+            if (max <= board.get(key)) ans.add(key);
+        }
+        
+        answer = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            answer[i] = ans.get(i);
+        }
+        
+        return answer;
+    }
+}
