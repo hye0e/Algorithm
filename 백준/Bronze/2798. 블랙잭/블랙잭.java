f9b4edb9b2c1a9d120a51281606d8e20dea3ccb0
HyeYoung Shin <nanda5050@naver.com>
2023-09-30 21:28:32 +0900

    [Bronze II] Title: 블랙잭, Time: 128 ms, Memory: 14020 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] cards = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
+        }
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    int total = cards[i] + cards[j] + cards[k];
+                    if (total <= M && total > answer) {
+                        answer = total;
+                    }
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
