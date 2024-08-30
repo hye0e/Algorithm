314fd1a554784d5e20318a0e4b552619b4f2251d
HyeYoung Shin <nanda5050@naver.com>
2024-08-30 16:14:54 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.36 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int j = A.length - 1;
+        for (int i = 0; i < A.length; i++) {
+            answer += A[i] *B[j];
+            j--;
+        }
+
+        return answer;
+    }
+}
