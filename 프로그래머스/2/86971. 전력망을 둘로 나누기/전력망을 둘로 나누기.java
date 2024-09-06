65c6f35bf46e728033837b0b2f72bc990d54bc4d
HyeYoung Shin <nanda5050@naver.com>
2024-09-06 11:16:23 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 14.70 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,55 +1,47 @@
 import java.util.*;
 
 class Solution {
-    static List<List<Integer>> graph;
-    static boolean[] visited;
+    static List<Integer>[] graph;
     static int answer = Integer.MAX_VALUE;
-    public int solution(int n, int[][] wires) {
-        // 초기 세팅
-        for (int i = 0; i < wires.length; i++) {
+    public int solution(int n, int[][] wires) {  
+        graph = new ArrayList[n + 1];
+        for (int i = 0; i < n; i++) {
             bfs(i, n, wires);
         }
-        
-        
         return answer;
     }
     
-    public static void bfs(int idx, int n, int[][] wires) {
-        graph = new ArrayList<>();
-        
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
+    public void bfs(int cut, int n, int[][] wires) {
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<Integer>();
         }
         
         for (int i = 0; i < wires.length; i++) {
-            if (i == idx) continue; // 전력망 끊기
+            if (i  == cut) continue;
             
-            int a = wires[i][0];
-            int b = wires[i][1];
+            int node = wires[i][0];
+            int vertex = wires[i][1];
             
-            graph.get(a - 1).add(b - 1);
-            graph.get(b - 1).add(a - 1);
+            graph[node].add(vertex);
+            graph[vertex].add(node);
         }
         
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(0);
-        
-        visited = new boolean[n];
-        visited[0] = true;
+        boolean[] visited = new boolean[n + 1];
+        visited[1] = true;
+        queue.add(1);
         
         while (!queue.isEmpty()) {
             int cur = queue.poll();
-            
-            for (int node : graph.get(cur)) {
-                if (!visited[node]) {
-                    visited[node] = true;
-                    queue.offer(node);
-                }
+            for (int node : graph[cur]) {
+                if (visited[node]) continue;
+                visited[node] = true;
+                queue.offer(node);
             }
         }
         
-        int one = 0, two = 0;
-        for (int i = 0; i < n; i++) {
+         int one = 0, two = 0;
+        for (int i = 1; i <= n; i++) {
             if (visited[i]) one++;
             else two++;
         }
