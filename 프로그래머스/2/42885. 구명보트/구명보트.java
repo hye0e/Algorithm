b4af5358cff8eedd734fbc3f11a6ba7c7291393b
HyeYoung Shin <nanda5050@naver.com>
2024-09-06 15:13:04 +0900

    [level 2] Title: 구명보트, Time: 13.50 ms, Memory: 56 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,20 +3,25 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
+        // 50 50 70 80
         
-        // 최대한 적게
         Arrays.sort(people);
         
-        int index = 0;
-        for (int i = people.length - 1; i >= index; i--) {
-            if (people[i] + people[index] <= limit) {
-                index++;
-                answer++;
-            } else {
-                answer++;
+        for (int i = 0; i < people.length; i++) {
+            for (int j = i + 1; j < people.length; j++) {
+                if (people[i] == 0) continue;
+                if (people[i] + people[j] <= limit) {
+                    people[i] = 0;
+                    people[j] = 0;
+                    answer++;
+                    System.out.println("?");
+                }
             }
         }
-       
+        
+        for (int i = 0; i < people.length; i++) {
+            if (people[i] != 0) answer++;
+        }
         return answer;
     }
 }
