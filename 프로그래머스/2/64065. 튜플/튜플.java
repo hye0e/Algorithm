0c3790cbcd19da03c0f4c4d53f14184f85162dce
HyeYoung Shin <nanda5050@naver.com>
2024-09-22 13:23:25 +0900

    [level 2] Title: 튜플, Time: 64.69 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,37 +2,43 @@ import java.util.*;
 
 class Solution {
     public List<Integer> solution(String s) {
-        List<Integer> answer = new ArrayList<>();
-        
-        // 1. 맨앞에 있는 {{ 제거
-        s = s.substring(2, s.length());
-        
-        // 2. 맨 뒤에 있는 }} 제거
-        s = s.substring(0, s.length() - 2);
-        
-        s = s.replace("},{", "-");
-        String[] arr = s.split("-");
-        
-        Arrays.sort(arr, new Comparator<String>() {
+        //1. 문자열을 배열로 변경
+        //2. 문자길이대로 정렬
+        //3. 하나씩 배열에 넣어보기
+        List<String[]> tuple = toConvert(s);
+        Collections.sort(tuple, new Comparator<String[]>() {
             @Override
-            public int compare(String o1, String o2) {
-                return Integer.compare(o1.length(), o2.length());
+            public int compare(String[] o1, String[] o2) {
+                return o1.length - o2.length;
             }
         });
         
+        int index = 0;
         
-        // 3. 탐색
-        for (String str : arr) {
-            String[] temp = str.split(",");
-            for (int i = 0; i < temp.length; i++) {
-                int num = Integer.parseInt(temp[i]);
-                if (!answer.contains(num)) {
-                    answer.add(num);
-                }
+        List<Integer> answer = new ArrayList<>();
+        for (String[] str : tuple) {
+            for (int i = 0; i < str.length; i++) {
+                int num = Integer.parseInt(str[i]);
+                if (!answer.contains(num)) answer.add(num);
+                else continue;
             }
         }
         
-        System.out.println(answer);
         return answer;
+    }
+    
+    public List<String[]> toConvert(String s) {
+        String temp = s.substring(2, s.length() - 2);
+        String temp2 = temp.replace("},{","/");
+        String[] temp3 = temp2.split("/");
+        
+        List<String[]> tuple = new ArrayList<>();
+        
+        for (String str : temp3) {
+            String[] temp4 = str.split(",");
+            tuple.add(temp4);
+        }
+        
+        return tuple;
     }
 }
