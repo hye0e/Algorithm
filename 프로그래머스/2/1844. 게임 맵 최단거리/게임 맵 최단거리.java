41b3b206449d1e0f66df2ae5ca52f8e30071b91d
HyeYoung Shin <nanda5050@naver.com>
2024-10-18 15:51:54 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.44 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,51 +1,66 @@
 import java.util.*;
 
 class Solution {
+    static int[] dx = new int[]{-1, 1, 0, 0};
+    static int[] dy = new int[]{0, 0, 1, -1};
     static boolean[][] visited;
-    static int n, m;
-    static int[][] graph;
+    static int N;
+    static int M;
+    
     public class Node {
         int x;
         int y;
+        int cnt;
         
-        public Node(int x, int y) {
+        public Node(int x, int y, int cnt) {
             this.x = x;
             this.y = y;
+            this.cnt = cnt;
         }
     }
     public int solution(int[][] maps) {
-        int answer = 0;
-        n = maps.length;
-        m = maps[0].length;
-        visited = new boolean[n][m];
-        graph = new int[n][m];
-       
-        bfs(0, 0, maps);
+        int start_x = 0;
+        int start_y = 0;
         
-        return graph[n - 1][m - 1] == 0 ? -1 : graph[n - 1][m - 1];
-     }
+        N = maps.length;
+        M = maps[0].length;
+        visited = new boolean[N][M];
+        
+        int answer = bfs(start_x, start_y, maps);
+        if (!visited[N - 1][M - 1]) return -1;
+        return answer;
+    }
     
-    public void bfs(int x, int y, int[][] maps) {
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
+    public int bfs(int start_x, int start_y, int[][] maps) {
         Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(x, y));
-        visited[x][y] = true;
-        graph[x][y] = 1;
-        while(!queue.isEmpty()) {
+        queue.add(new Node(start_x, start_y, 1));
+        visited[start_x][start_y] = true;
+        int total = 0;
+        while (!queue.isEmpty()) {
             Node node = queue.poll();
+            int x = node.x;
+            int y = node.y;
+            int cnt = node.cnt;
+            
+            // 목표 지점에 도달한 경우 해당 거리 반환
+            if (x == N - 1 && y == M - 1) {
+                return cnt;
+            }
+
             for (int i = 0; i < 4; i++) {
-                int nx = node.x + dx[i];
-                int ny = node.y + dy[i];
-                if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                
+                if (0 <= nx && nx < N && 0 <= ny && ny < M) {
                     if (visited[nx][ny]) continue;
                     if (maps[nx][ny] == 0) continue;
-                    queue.add(new Node(nx, ny));
+                    
                     visited[nx][ny] = true;
-                    graph[nx][ny] = graph[node.x][node.y] + 1;
+                    queue.add(new Node(nx, ny, cnt + 1));
                 }
             }
         }
         
+        return -1;
     }
 }
