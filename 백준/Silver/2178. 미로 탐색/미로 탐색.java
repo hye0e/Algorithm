7fd785088ed619750464e6d789ab9cfc65662814
HyeYoung Shin <nanda5050@naver.com>
2025-02-04 23:55:15 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14688 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,81 +1,67 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Pair {
-	int x, y;
-
-	public Pair(int x, int y) {
-		super();
-		this.x = x;
-		this.y = y;
-	}
-
-	@Override
-	public String toString() {
-		return "Node [x=" + x + ", y=" + y + "]";
-	}
-}
 public class Main {
+    static int[][] graph;
+    static boolean[][] visited;
+    static int N;
+    static int M;
+    static int[] dx = new int[]{-1, 1, 0, 0};
+    static int[] dy = new int[]{0, 0, 1, -1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	static StringTokenizer st;
-	static int N, M;
-	static int answer;
-	static int[][] graph;
-	static boolean[][] isVisited;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-	
-		graph = new int[N][M];
-		isVisited = new boolean[N][M];
-		
-		for (int i = 0; i < N; i++) {
-			String temp = br.readLine();
-			String[] tempArr = temp.split("");
-			for (int j = 0; j < M; j++) {
-				graph[i][j] = Integer.parseInt(tempArr[j]);
-			}
-		}
-		
-		bfs();
-		System.out.println(graph[N-1][M-1]);
-	}
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-	public static void bfs() {
-		answer = 0;
-		Queue<Pair> queue = new LinkedList<>();
-		queue.offer(new Pair(0, 0));
-		isVisited[0][0] = true;
-		
-		while (!queue.isEmpty()) {
-			Pair p = queue.poll();
-			int x = p.x;
-			int y = p.y;
-			
-			int[] dx = {-1, +1, 0, 0};
-			int[] dy = {0, 0, -1, +1};
-			
-			for (int i = 0; i < 4; i++) {
-				int nx = x + dx[i];
-				int ny = y + dy[i];
+        graph = new int[N][M];
+        visited = new boolean[N][M];
 
-				if (0 <= nx && nx < N && 0 <= ny && ny < M) {
-					if (graph[nx][ny] == 1 && !isVisited[nx][ny]) {
-						graph[nx][ny] = graph[x][y] + 1;
-						isVisited[nx][ny] = true;
-						queue.offer(new Pair(nx, ny));
-					}
-				}
-			}
-		}
-	}
+        for (int i = 0; i < N; i++) {
+            String temp = br.readLine();
+            for (int j = 0; j < M; j++) {
+                graph[i][j] = temp.charAt(j) - '0';
+            }
+        }
+
+        System.out.println(bfs(0, 0) + 1);
+    }
+
+    public static int bfs(int startX, int startY) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{startX, startY, 1});
+        visited[startX][startY] = true;
+
+        while(!queue.isEmpty()) {
+            int[] temp = queue.poll();
+            int x = temp[0];
+            int y = temp[1];
+            int cnt = temp[2];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                //
+                if (nx == N - 1 && ny == M - 1) {
+                    return cnt;
+                }
+
+                if (0 <= nx && nx < N && 0 <= ny && ny < M) {
+                    if (graph[nx][ny] == 1 && !visited[nx][ny]) {
+                        visited[nx][ny] = true;
+                        queue.add(new int[]{nx, ny, cnt + 1});
+                    }
+                }
+
+            }
+
+        }
+        return -1;
+    }
 }
