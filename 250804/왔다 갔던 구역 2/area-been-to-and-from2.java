de3192f412963452ea563f9681d43b8cf5b23c2e
HyeYoung Shin <nanda5050@naver.com>
2025-08-04 21:56:32 +0900

    Title: 왔다 갔던 구역 2, Time: 89ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250804/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        // Please write your code here.
+        int[] count = new int[200];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int now = 100;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int go = Integer.parseInt(st.nextToken());
+            String direction = st.nextToken();
+        
+            if (direction.equals("R")) {
+                int last = now;
+                for (int j = now; j < now + go; j++) {
+                    // System.out.println("R -> " + j);
+                    count[j]++;
+                    // now++;
+                    last = j;
+                }
+                now = last;
+                // System.out.println("현재) " + now);
+
+            } else {
+                int last = now;
+
+                for (int j = now; j > now - go; j--) {
+                    // System.out.println("L -> " + j);
+                    count[j]++;
+                    // now--;
+                    last = j;
+                }
+                now = last;
+                // System.out.println("현재) " + now);
+
+            }
+        }
+        int answer = 0;
+        for (int i = 0; i < 200; i++) {
+            if (count[i] >= 2) answer++;
+        }
+        System.out.println(answer + 1);
+    }
+}
