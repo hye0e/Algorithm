dc1fd33734f476235ceda2e1ebf08e95eef3aa22
HyeYoung Shin <nanda5050@naver.com>
2026-01-22 19:26:52 +0900

    [level 2] Title: 최솟값 만들기, Time: 3.81 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -5,23 +5,17 @@ class Solution
     public int solution(int []A, int []B)
     {
         int answer = 0;
-        int sum = Integer.MAX_VALUE;
-        // 1, 2, 4 / 4,4,5
-        // 1*5 2*4 4*4 = 5 + 8 + 16 = 29
-        
-        // 1, 2 / 3, 4
-        // 1*4 + 2
-        Integer[] b2 = new Integer[B.length];
-        for (int i = 0; i < B.length; i++) {
-            b2[i] = new Integer(B[i]);
-        }
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
         Arrays.sort(A);
-        Arrays.sort(b2, Collections.reverseOrder());
-        // 0, 1, 2
-        for (int i = 0; i < A.length; i++) {
-            answer += A[i] * b2[i];
-        }
+        Arrays.sort(B);
         
+        int N = A.length;
+        for (int i = 0; i < N; i++) {
+            int temp = N - i - 1;
+            answer += A[i] * B[temp];
+        }
+
         return answer;
     }
 }
