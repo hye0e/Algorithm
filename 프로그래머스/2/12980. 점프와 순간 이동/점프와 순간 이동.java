6df50302602d431a3d8b1fb7a6700e4329532950
HyeYoung Shin <nanda5050@naver.com>
2026-01-26 20:33:31 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -3,15 +3,18 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int ans = 0;
-        
-        while(n != 0){
-            if(n % 2 == 0){
-                n /= 2;
-            } else {
-                n--;
+
+        while (n != 0) {
+            // 홀수
+            if (n % 2 != 0) {
                 ans++;
+                n--;
+            } else {
+                n -= (n / 2);
             }
         }
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        // System.out.println("Hello Java");
 
         return ans;
     }
